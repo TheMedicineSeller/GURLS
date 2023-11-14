022b6f166d9e7ccb39701b855ebde33589a1648e
TheMedicineSeller <nishanth21902@gmail.com>
2023-11-14 15:10:18 +0530

    Port var changed from "3000" to ":3000" since using raw variables

--- api/config/config.go
+++ api/config/config.go
@@ -2,7 +2,7 @@ package config
 
 var DB_ADDR = "db:6379"
 var DB_PASSWORD = ""
-var PORT = "3000"
+var PORT = ":3000"
 var DOMAIN = "localhost:3000"
 // For rate limiting purposes
 var API_LIMIT = 8
